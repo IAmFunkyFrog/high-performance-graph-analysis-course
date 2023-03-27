db56dace0086a1afbbb88f5d7599918b8e571bb1
Stepan Trefilov <strefilov@azul.com>
2023-03-27 13:25:02 +0300

    Added comments for task 3

--- project/triangles.py
+++ project/triangles.py
@@ -12,6 +12,12 @@ __all__ = [
 
 
 def triangles_count_for_each_vertex(graph: Graph) -> list[int]:
+    """
+    Returns count of triangles for each node
+
+    @param graph: graph to compute count of triangles
+    @return: list where for each vertex computed count of triangles in which this node participates
+    """
     if len(graph.nodes) == 0:
         return []
 
@@ -22,6 +28,12 @@ def triangles_count_for_each_vertex(graph: Graph) -> list[int]:
 
 
 def triangles_count_for_each_vertex_matrix(graph: Matrix) -> Vector:
+    """
+    Returns count of triangles for each node
+
+    @param graph: adjacency matrix of graph to compute count of triangles
+    @return: vector where for each vertex computed count of triangles in which this node participates
+    """
     squared = graph.mxm(graph, mask=graph, desc=pgb.descriptor.S)
 
     result = squared.reduce_vector()
@@ -29,6 +41,12 @@ def triangles_count_for_each_vertex_matrix(graph: Matrix) -> Vector:
 
 
 def triangles_count_cohen(graph: Graph) -> int:
+    """
+    Returns count of triangles in graph
+
+    @param graph: graph to compute count of triangles
+    @return: count of triangles in graph
+    """
     if len(graph.nodes) == 0:
         return 0
 
@@ -37,11 +55,23 @@ def triangles_count_cohen(graph: Graph) -> int:
 
 
 def triangles_count_cohen_matrix(graph: Matrix) -> int:
+    """
+    Returns count of triangles in graph
+
+    @param graph: adjacency matrix of graph to compute count of triangles
+    @return: count of triangles in graph
+    """
     result = graph.tril().mxm(graph.triu(), mask=graph, desc=pgb.descriptor.S)
     return result.reduce() // 2
 
 
 def triangles_count_sandia(graph: Graph) -> int:
+    """
+    Returns count of triangles in graph
+
+    @param graph: graph to compute count of triangles
+    @return: count of triangles in graph
+    """
     if len(graph.nodes) == 0:
         return 0
 
@@ -50,6 +80,12 @@ def triangles_count_sandia(graph: Graph) -> int:
 
 
 def triangles_count_sandia_matrix(graph: Matrix) -> int:
+    """
+    Returns count of triangles in graph
+
+    @param graph: adjacency matrix of graph to compute count of triangles
+    @return: count of triangles in graph
+    """
     tril = graph.tril()
     result = tril.mxm(tril, mask=tril, desc=pgb.descriptor.S)
     return result.reduce()
